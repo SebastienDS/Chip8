62da73a314e7dfe807d86f5614afc8f9f6c2f6a2
SebastienDS <47292880+SebastienDS@users.noreply.github.com>
2022-09-01 14:27:13 +0200

    Add Main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,28 @@
+import sys
+import logging
+import pygame
 from pathlib import Path
 from emulator.Chip8 import Chip8
 from emulator.Rom import Rom
 
-tetris = Rom(Path("roms", "TETRIS"))
+pygame.init()
+logging.basicConfig(level=logging.WARNING)
+
+TIMER_EVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(TIMER_EVENT, 15)
+
+
+rom = Rom(Path("roms", sys.argv[1]))
 
 chip8 = Chip8()
-chip8.load_rom(tetris)
-print(chip8.memory)
+chip8.load_rom(rom) 
+
+while True:
+    chip8.step()
+
+    for event in pygame.event.get():
+        if event.type in { pygame.QUIT, pygame.K_ESCAPE }:
+            exit()
+        elif event.type == TIMER_EVENT:
+            chip8.update_timers()
+    pygame.time.wait(1)
